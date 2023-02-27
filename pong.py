a92f8923b39a689762492dba3f182d0eafb6cb6b
stenio bomfim <stenio_lima2016@hotmail.com>
2023-02-27 06:32:21 -0300

    Frist commite

---
+++ pong.py
@@ -0,0 +1,84 @@
+import pygame 
+pygame.init()
+
+WIDTH, HEIGHT = 700, 500 
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("pong")
+
+############## colors(cores) ###############
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+PADDLE_WIDTH, PADDLE_HIEGHT = 20, 100
+############# Class Paddle ( class das pas) ##########
+class Paddle:
+    COLOR = WHITE
+    VEL = 4
+
+    def __init__(self,x,y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+    
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
+class Ball:
+    def __init__(self, x,y, radius):
+        pass
+
+def handle_paddle_movement(keys, left_paddle,right_paddle):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+
+############### drawings of game objects (Desenhos dos objetos do jogos) ###################
+def draw(win, paddles):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+
+
+    pygame.display.update()
+
+
+################ run game windows ##################
+def main():
+    run = True
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HIEGHT//2,PADDLE_WIDTH, PADDLE_HIEGHT)
+    rigth_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HIEGHT//2, PADDLE_WIDTH, PADDLE_HIEGHT)
+    while run:
+        draw(WIN, [left_paddle,rigth_paddle])
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, rigth_paddle)
+
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
